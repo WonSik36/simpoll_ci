96af013ef7847b1f5cb7b0cec915af49a0c9a76e
wonsik <cws0173@gmail.com>
2020-02-06 20:09:53 +0900

    opentutorials online lecture https://opentutorials.org/course/697/3828

--- application/controllers/Topic.php
+++ application/controllers/Topic.php
@@ -1,16 +1,23 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Topic extends CI_Controller {
-
-	function index(){
-        $this->load->view('head');
-        $this->load->view('topic');
+    function __construct()
+    {       
+        parent::__construct();
+        $this->load->database();
+		$this->load->model('topic_model');
+		
+		$this->load->view('head');
+        $topics = $this->topic_model->gets();
+        $this->load->view('topic_list', array('topics'=>$topics));
+    }
+    function index(){        
+        $this->load->view('main');
         $this->load->view('footer');
     }
-    function get($id){
-        $this->load->view('head');
-        $this->load->view('get', array('id'=>$id));
+    function get($id){        
+        $topic = $this->topic_model->get($id);
+        $this->load->view('get', array('topic'=>$topic));
         $this->load->view('footer');
     }
 }
+?>
