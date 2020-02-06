b868aeb5e1e88d292345a71f928dc35b9d30bad1
wonsik <cws0173@gmail.com>
2020-02-06 15:27:12 +0900

    code igniter

---
+++ application/controllers/Topic.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Topic extends CI_Controller {
+
+	function index(){
+        $this->load->view('head');
+        $this->load->view('topic');
+        $this->load->view('footer');
+    }
+    function get($id){
+        $this->load->view('head');
+        $this->load->view('get', array('id'=>$id));
+        $this->load->view('footer');
+    }
+}
