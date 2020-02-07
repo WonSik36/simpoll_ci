cb8608f5b93ed11e428f79f6553a4505a91a6882
wonsik <cws0173@gmail.com>
2020-02-07 13:25:00 +0900

    update user model

--- application/models/model/User_model.php
+++ application/models/model/User_model.php
@@ -26,5 +26,54 @@ class User_model extends CI_Model {
         insert
         param: 배열(사용자)
     */
+    function insert($user){
+        $sql = "INSERT INTO sp_user (`email`,`name`,`password`) VALUES (?,?,?)";
+        $this->db->query($sql, array($user['email'], $user['name'], $user['password']));
+    }
+
+    /*
+        selectRoomUserList
+        해당 방에 포함되어있는 유저 반환
+        param: 방 아이디
+        return: 유저 배열
+    */
+    function selectRoomUserList($room_id){
+        $sql  = "SELECT sp_room_user.room_id, sp_room_user.user_id, sp_user.email, sp_user.name"
+        $sql .= " FROM sp_room_user INNER JOIN sp_user ON sp_room_user.user_id = sp_user.id"
+        $sql .= " WHERE sp_room_user.room_id = ?";
+        
+        return $this->db->query($sql, array($room_id))->result_array();
+    }
+
+
+    /*
+        login
+        param: 유저 email, password
+        return: 유저 object
+    */
+    function login($email, $pw){
+        $sql = "SELECT * FROM sp_user WHERE email=? AND password=?";
+        return $this->db->query($sql,array($email,$pw))->row();
+    }
+    
+    
+    /*
+        update
+        param: 배열(사용자)
+    */
+    function update($user){
+        $sql = "UPDATE sp_user SET email = ?, name = ?, password =? WHERE id=?";
+        $this->db->query($sql, array($user['email'],$user['name'],$user['password'],$user['id']));
+    }
+
+    /*
+        delete
+        사용자 삭제(update)
+        param: 사용자 id
+    */
+    function delete($user_id){
+        $sql = "UPDATE sp_user SET deleted=1 WHERE id=?";
+        $this->db->query($sql, array($user_id));
+    }
 }
 ?>
