c1b89d6f43bf64de57068f15fe4827db1b3b1f39
wonsik <cws0173@gmail.com>
2020-02-11 18:48:44 +0900

    add logout function

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -53,18 +53,10 @@ class User extends CI_Controller {
           $this->load->view('sign_up');
       }
     }
-    function index(){
-        echo '
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset="utf-8"/>
-            </head>
-            <body>
-                토픽 메인 페이지
-            </body>
-        </html>
-        ';
+
+    function logout(){
+        $this->session->sess_destroy();
+        $this->load->view('result',array('message'=>"로그아웃 되었습니다.", 'location'=>"/index.php/home"));
     }
 }
 ?>
