2d9f23c667fc750daf56dbcb7e28a5f8203af2df
wonsik <cws0173@gmail.com>
2020-02-11 18:44:23 +0900

    update home page
    refactoring head

--- application/views/head.php
+++ application/views/head.php
@@ -1,6 +1,58 @@
-<!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset="utf-8"/>
-            </head>
-            <body>
+<?php
+    if(empty($nickname)){
+      ?>
+<div class="header">
+    <span class="title">
+        <a href="index.html">Simpoll</a>
+    </span>
+    <ul class="topnav">
+        <li class="right">
+            <a href="make_room.html">새로운 방 만들기</a>
+        </li>
+        <li class="right">
+            <a href="list_room.html">참여한 방 목록</a>
+        </li>
+        <li class="right">
+            <a href="list_room.html">개설한 방 목록</a>
+        </li>
+        <li class="right">
+            <a href="login.html">로그인</a>
+        </li>
+        <li class="right">
+            <a href="sign_up.html">회원가입</a>
+        </li>
+    </ul>
+</div>
+<?php
+    }else{
+      ?>
+<div class="header">
+    <span class="title">
+        <a href="index.html">Simpoll</a>
+    </span>
+    <ul class="topnav">
+        <li class="right">
+            <a href="make_room.html">새로운 방 만들기</a>
+        </li>
+        <li class="right">
+            <a href="list_room.html">참여한 방 목록</a>
+        </li>
+        <li class="right">
+            <a href="list_room.html">개설한 방 목록</a>
+        </li>
+        <li id="icon">
+            <i class="fas fa-user-circle"></i>
+        </li>
+        <li id="info" class="right"><?=$nickname?>
+            님</li>
+        <li class="right">
+            <a href="#">마이페이지</a>
+        </li>
+        <li class="right">
+            <a href="/index.php/user/logout">로그아웃</a>
+        </li>
+    </ul>
+</div>
+<?php
+    }
+    ?>
--- application/views/home.php
+++ application/views/home.php
@@ -102,39 +102,7 @@ input[type=submit]{
 </head>
 <body>
     <!-- navigation -->
-    <?php
-    if(empty($nickname)){
-      ?>
-      <div class="header">
-        <span class="title"><a href="index.html">Simpoll</a></span>
-        <ul class="topnav">
-          <li class="right"><a href="make_room.html">새로운 방 만들기</a></li>
-          <li class="right"><a href="list_room.html">참여한 방 목록</a></li>
-          <li class="right"><a href="list_room.html">개설한 방 목록</a></li>
-          <li class="right"><a href="login.html">로그인</a></li>
-          <li class="right"><a href="sign_up.html">회원가입</a></li>
-        </ul>
-      </div>
-      <?php
-    }else{
-      ?>
-      <div class="header">
-  <span class="title"><a href="index.html">Simpoll</a></span>
-  <ul class="topnav">
-    <li class="right"><a href="make_room.html">새로운 방 만들기</a></li>
-    <li class="right"><a href="list_room.html">참여한 방 목록</a></li>
-    <li class="right"><a href="list_room.html">개설한 방 목록</a></li>
-    <li id="icon"><i class="fas fa-user-circle"></i></li>
-    <li id="info" class="right"><?=$nickname?> 님</li>
-    <li class="right"><a href="#">마이페이지</a></li>
-  </ul>
-</div>
-    <?php
-    }
-    ?>
-
-
-
+    <?php include 'head.php';?>
 
     <!-- contents -->
     <div>
