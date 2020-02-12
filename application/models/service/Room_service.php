87a6798979bd5318112bc8cd3473a32188ec7fb4
wonsik <cws0173@gmail.com>
2020-02-12 13:31:08 +0900

    add room service

---
+++ application/models/service/Room_service.php
@@ -0,0 +1,27 @@
+<?php
+
+class Room_service extends CI_Model {
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('dao/room_model');
+    }
+
+    /*
+        insert
+        param: 배열(사용자)
+    */
+    function register($user){
+        return $this->room_model->insert($user);
+    }
+
+    /*
+        login
+        param: 유저 email, password
+        return: 유저 object
+    */
+    function speacker_list($email, $pw){
+        return $this->user_model->login($email, $pw);
+    }
+}
+?>
