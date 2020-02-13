07e5f10dc059701ddd6af4123c26582ecb9e7c30
wonsik <cws0173@gmail.com>
2020-02-13 15:29:13 +0900

    update vote.php

---
+++ application/views/vote.php
@@ -0,0 +1,24 @@
+<!-- vote tab -->
+<!-- vote header -->
+<div class="vote-tab">
+    <ul class="vote-ul">
+        <li class="check_icon">
+            <i class="far fa-check-square"></i>
+        </li>
+        <li class="title">점심식사<br>참여인원 13명</li>
+        <li>
+            <i class="far fa-thumbs-up"></i>
+        </li>
+        <li>
+            <i class="far fa-thumbs-down"></i>
+        </li>
+        <li class="right">2020-03-10</li>
+    </ul>
+</div>
+<!-- vote contents -->
+<div class="contents">
+    <div class="vote">
+            <p>투표 결과</p>
+            <img src="https://www.meta-chart.com/assets/images/pie/pie_value.png" alt="" width="500px">
+    </div>
+</div>
