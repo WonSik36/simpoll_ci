6f39be45f994450e710dd1532eddad84e8591a39
wonsik <cws0173@gmail.com>
2020-03-31 10:37:17 +0900

    update frontend

---
+++ precache-manifest.293e91b314eb509b87579e6e1ed8e8ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385283272452a8c5d0fd7afc58656d27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc055fcb6e59958e18f6",
+    "url": "/static/css/main.8a00efb2.chunk.css"
+  },
+  {
+    "revision": "63f367a5a30dcf44d62f",
+    "url": "/static/js/2.72d39ab4.chunk.js"
+  },
+  {
+    "revision": "a2e26c9252dea7ee90077b51746a1553",
+    "url": "/static/js/2.72d39ab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc055fcb6e59958e18f6",
+    "url": "/static/js/main.bd35c4ab.chunk.js"
+  },
+  {
+    "revision": "c335029f174ee6486d1a",
+    "url": "/static/js/runtime-main.0833a1f3.js"
+  }
+]);
